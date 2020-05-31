4227c0091cf4ee857554ac024645a32fbbd6b8e1
rama-venna <rama.s.venna@gmail.com>
2020-05-30 22:43:13 -0400

    Adding generics demo classes

--- src/main/java/com/abonado/genericsdemo/BoundedTypeParameterDemo.java
+++ src/main/java/com/abonado/genericsdemo/BoundedTypeParameterDemo.java
@@ -1,7 +1,29 @@
 package com.abonado.genericsdemo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BoundedTypeParameterDemo {
     public static void main(String[] args) {
-
+        GenericsDemo<List> gd = new GenericsDemo<>();
+        gd.set(Arrays.asList(1, 2, "String"));
+        System.out.println(gd.get());
+        System.out.println(gd.getSize());
     }
+}
+
+class GenericsDemo<T extends List>{
+    private T t;
+
+    public void set(T t){
+        this.t = t;
+    }
+    public int getSize(){
+        return t.size();
+    }
+
+    public T get(){
+        return t;
+    }
+
 }
